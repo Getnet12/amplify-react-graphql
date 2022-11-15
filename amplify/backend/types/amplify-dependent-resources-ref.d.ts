f0b71dfa151e3a2dfcc4d764c12ba40ca5516dd6
Getnet Mekuriyaw <gmekuriy@08f8bc77653e.ant.amazon.com>
2022-11-14 23:20:14 -0600

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyreactgraphqle15b2457": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
